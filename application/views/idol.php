877cb31636d5ef38c7983453c52f3aa0886e36f3
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2019-07-23 17:58:05 +0900

    Update idol.php

--- application/views/idol.php
+++ application/views/idol.php
@@ -195,7 +195,7 @@ $fhme_linkname = $idol->slug_fhme."/".$fhme_linknames[1]."-".$fhme_linknames[0];
         <h3>Fediverse</h3>
         <div class="buttonbox">
             <a href="https://twista.283.cloud/search?q=<?= $urlname ?>" class="button il trisection" target="_blank">
-                twista でタグ検索
+                twista でキーワード検索
             </a>
             <a href="https://imastodon.net/tags/<?= $urlname ?>" class="button il trisection" target="_blank">
                 im@stodon でタグ検索
